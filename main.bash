0f8700f3116c42df6e90b3fff697a62fbe46428b
Ekaterina K <34368106+riarurainey@users.noreply.github.com>
2022-12-01 16:02:29 +0300

    Update main.bash

--- main.bash
+++ main.bash
@@ -7,10 +7,20 @@ number="${user_input[1]}"
 
 rn='^[+-]?[0-9]+\.?[0-9]*$'
 rs='[a-z]+_to_[a-z]+'
+float='^[0-9]+$'
+int='^[+-]?[0-9]+\.?[0-9]*$'
 
 
 if [[ $string =~ $rs ]] && [[ $number =~ $rn ]] && [[ $arr_length = 2 ]]; then
-    echo "The definition is correct!"
+    echo "Enter a value to convert:"
+    read value
+    while [[ ! $value =~ $float ]] && [[ ! $value =~ $int ]]
+    do
+        echo "Enter a float or integer value!"
+        read value
+    done
+    Result=$(bc -l <<<$number*$value)
+    echo "Result: $Result"
 else
     echo "The definition is incorrect!"
 fi
